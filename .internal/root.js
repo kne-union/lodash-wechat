ea34dcf5fcacbb2bc66556183fe5dc2af1199911
zhipenglin <sunandmoon001@163.com>
2023-06-29 10:10:05 +0800

    修正微信小程序中lodash Global获取不到的问题

--- .internal/root.js
+++ .internal/root.js
@@ -8,6 +8,20 @@ const freeGlobalThis = typeof globalThis === 'object' && globalThis !== null && 
 const freeSelf = typeof self === 'object' && self !== null && self.Object === Object && self
 
 /** Used as a reference to the global object. */
-const root = freeGlobalThis || freeGlobal || freeSelf || Function('return this')()
+const root = freeGlobalThis || freeGlobal || freeSelf || {
+    Array: Array,
+    Date: Date,
+    Error: Error,
+    Function: Function,
+    Math: Math,
+    Object: Object,
+    RegExp: RegExp,
+    String: String,
+    TypeError: TypeError,
+    setTimeout: setTimeout,
+    clearTimeout: clearTimeout,
+    setInterval: setInterval,
+    clearInterval: clearInterval
+} || Function('return this')();
 
 export default root
