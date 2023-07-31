1c064c0cbfc355c0f00f6791448f40cd330e0efc
zhipenglin <sunandmoon001@163.com>
2023-07-31 16:19:20 +0800

    修改global在微信报错问题

--- _root.js
+++ _root.js
@@ -4,6 +4,20 @@ import freeGlobal from './_freeGlobal.js';
 var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
 
 /** Used as a reference to the global object. */
-var root = freeGlobal || freeSelf || Function('return this')();
+var root = freeGlobal || freeSelf || {
+  Array: Array,
+  Date: Date,
+  Error: Error,
+  Function: Function,
+  Math: Math,
+  Object: Object,
+  RegExp: RegExp,
+  String: String,
+  TypeError: TypeError,
+  setTimeout: setTimeout,
+  clearTimeout: clearTimeout,
+  setInterval: setInterval,
+  clearInterval: clearInterval
+} || Function('return this')();
 
 export default root;
